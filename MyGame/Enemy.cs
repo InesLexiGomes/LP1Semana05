99dbfaabdd31fcb625f9870490202372098aca31
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-21 22:51:37 +0000

    Added name getter and damage method

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -11,6 +11,25 @@ namespace MyGame
         private float health;
         private float shield;
 
+        // Getter de nome
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Metodo para dar dano
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
         // Construtor
         public Enemy(string name)
         {
