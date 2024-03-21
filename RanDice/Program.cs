454ba04806a11a314101956de8c16592140f5a3f
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-21 22:32:33 +0000

    random dicerolls

---
+++ RanDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(args[0]);
+            int seed = int.Parse(args[1]);
+
+            Random rand = new Random(seed);
+
+            int total = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                total += rand.Next(1,7);
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
